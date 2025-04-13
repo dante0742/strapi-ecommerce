2a03318f025cf90c4d530b13cca403399074df5c
dante0742 <99133701+dante0742@users.noreply.github.com>
2025-04-13 13:37:20 +0000

    Update middlewares.js

--- config/middlewares.js
+++ config/middlewares.js
@@ -3,7 +3,7 @@ module.exports = [
   {
     name: 'strapi::cors',
     config: {
-      origin: ['http://localhost:5500', 'https://localhost:5501'],
+      origin: ['http://localhost:5500', 'https://localhost:5501', 'http://127.0.0.1:5501'],
       methods: ['GET', 'POST', 'PUT', 'DELETE'],
       headers: '*',
       credentials: true,
