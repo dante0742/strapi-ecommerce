2af97c529639ea7b2a4152156001f7448b357609
dante0742 <99133701+dante0742@users.noreply.github.com>
2025-04-13 13:09:20 +0000

    Update middlewares.js

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,6 +1,13 @@
 module.exports = [
   'strapi::logger',
   'strapi::errors',
+   config: {
+      origin: ['http://localhost:5500', 'https://localhost:5501'],
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+      headers: '*',
+      credentials: true,
+    },
+  },
   'strapi::security',
   'strapi::cors',
   'strapi::poweredBy',
