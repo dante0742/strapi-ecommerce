ee7d4e33fda940ffef67cf77470b3275488048da
dante0742 <99133701+dante0742@users.noreply.github.com>
2025-04-16 21:18:16 +0000

    Update plugins.js

--- config/plugins.js
+++ config/plugins.js
@@ -1,4 +1,6 @@
-module.exports = {
+const path = require("path");
+
+module.exports = ({ env }) => ({
   graphql: {
     enabled: true,
     config: {
@@ -9,4 +11,21 @@ module.exports = {
       amountLimit: 100,
     },
   },
-};
+  email: {
+    config: {
+      provider: "nodemailer",
+      providerOptions: {
+        host: env("SMTP_HOST"),
+        port: env("SMTP_PORT"),
+        auth: {
+          user: env("SMTP_USERNAME"),
+          pass: env("SMTP_PASSWORD"),
+        },
+      },
+      settings: {
+        defaultFrom: "no-reply@teramatt.com",
+        defaultReplyTo: "support@teramatt.com",
+      },
+    },
+  },
+});
